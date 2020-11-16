57c1f548c5a85341eabca4adca0d119541794247
Paulo Emilio dos Santos Abreu <pesabreu@gmail.com>
2020-11-16 10:55:59 -0300

    Acerto e finalização do desenvolvimento.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require('./server');
-const { port } = require('./.env');
 
-app.listen(port, () => {
+// eslint-disable-next-line no-undef
+let porta = process.env.PORT;
+if (!porta) {
+  let { port } = require('./.env');
+  porta = port;
+}
+
+app.listen(porta, () => {
   console.log('\n\n API em Operação.....');
 });
